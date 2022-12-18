d587bf3597b8e7286393abc736f0f731901603b3
husky022 <balashovia@bk.ru>
2022-12-18 18:40:04 +0300

    added googlesheets link

--- api_manager.py
+++ api_manager.py
@@ -6,7 +6,7 @@ import json
 задач по текущему аккаунту'''
 
 
-class ApiAmoManager():
+class ApiAmoManager:
 	def __init__(self, client_id, client_secret, code, redirect_uri, client_link):
 		self.client_id = client_id
 		self.client_secret = client_secret
@@ -30,20 +30,21 @@ class ApiAmoManager():
 
 	# обновление связки токенов
 	def refresh_access(self, refresh_token):
-		data = {'client_id': self.client_id,
-				'client_secret': self.client_secret,
-				'grant_type': 'refresh_token',
-				'redirect_uri': self.redirect_uri,
-				'refresh_token': refresh_token
+		data = {
+			'client_id': self.client_id,
+			'client_secret': self.client_secret,
+			'grant_type': 'refresh_token',
+			'redirect_uri': self.redirect_uri,
+			'refresh_token': refresh_token
 				}
 		url = self.client_link + '/oauth2/access_token'
 		request = requests.post(url, data=data)
 		return json.loads(request.text)
 
 	# получение списка задач по текущему аккаунту
-	def get_tasks_data(self, token_data):
+	@staticmethod
+	def get_tasks_data(token_data):
 		api_call_headers = {'Authorization': token_data['token_type'] + ' ' + token_data['access_token']}
 		url = secrets.client_link + '/api/v4/tasks'
 		api_statuses_response = requests.get(url, headers=api_call_headers, verify=True)
 		return api_statuses_response.json()
-
