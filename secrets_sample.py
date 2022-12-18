f7e4d1a4776a08392fdfb2970d7dee057f5b74ab
husky022 <balashovia@bk.ru>
2022-12-18 18:40:59 +0300

    added googlesheets link

--- secrets_sample.py
+++ secrets_sample.py
@@ -8,5 +8,5 @@ client_id = 'ID интеграции'
 redirect_url = 'ссылка для перенаправления, точно такая же как в настройках интеграции'
 client_link = 'ссылка на личный аккаунт пользователя'
 
-
+# ссылка на таблицу
 link = 'https://docs.google.com/spreadsheets/d/1HONYJ6ZhbTO3MqJD0t0co9aEpFtVr2W8dDKUisnJ-n0/edit?usp=sharing'
